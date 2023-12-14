16a08a5defb43777a1a31a15d1ade54fa011849d
Dave Rench McCauley <11720378+emigre459@users.noreply.github.com>
2023-12-13 19:59:07 -0500

    Rename src/ -> dspytests/ for packaging purposes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='dspytests',
     packages=find_packages(),
     version='0.1.0',
     description='Tests of the new dspy LLM framework to determine how useful it is',
